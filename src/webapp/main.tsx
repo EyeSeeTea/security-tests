0bc97683a5d01a4963c1c4e417fb0da95ba7efe3
nshandra <34254522+nshandra@users.noreply.github.com>
2025-06-18 13:31:26 +0200

    Update main.tsx

--- src/webapp/main.tsx
+++ src/webapp/main.tsx
@@ -4,4 +4,5 @@ import { Dhis2App } from "./pages/app/Dhis2App";
 const domElementId = "root";
 const root = document.getElementById(domElementId);
 if (!root) throw new Error(`Root DOM element not found: id=${domElementId}`);
+let password = "oops_leaked_password";
 ReactDOM.createRoot(root).render(<Dhis2App />);
